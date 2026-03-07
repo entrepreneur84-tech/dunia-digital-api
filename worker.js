c79e0abffe06d0ef0ff343884c9c3af24ab5c900
Entrepreneur <entrepreneur84@bk.ru>
2026-03-08 01:45:05 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,27 @@
 export default {
   async fetch(request, env) {
 
-    await env.ORDERS.put("test-order", "order pertama")
+    const url = new URL(request.url)
 
-    const data = await env.ORDERS.get("test-order")
+    if (url.pathname === "/health") {
+      return new Response(JSON.stringify({
+        status: "ok",
+        service: "dunia-digital-api"
+      }), {
+        headers: { "Content-Type": "application/json" }
+      })
+    }
 
-    return new Response("Data KV: " + data)
+    if (url.pathname === "/test-order") {
 
+      await env.ORDERS.put("order_001", "test order")
+
+      const data = await env.ORDERS.get("order_001")
+
+      return new Response("Order data: " + data)
+
+    }
+
+    return new Response("Dunia Digital API aktif")
   }
 }
