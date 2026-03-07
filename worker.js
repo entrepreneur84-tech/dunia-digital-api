991ed0d6e66b2e35761e5be673aa13353761c2b3
Entrepreneur <entrepreneur84@bk.ru>
2026-03-08 01:37:30 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,11 @@
 export default {
-  async fetch(request) {
-    return new Response("Dunia Digital API aktif");
+  async fetch(request, env) {
+
+    await env.ORDERS.put("test-order", "order pertama")
+
+    const data = await env.ORDERS.get("test-order")
+
+    return new Response("Data KV: " + data)
+
   }
 }
